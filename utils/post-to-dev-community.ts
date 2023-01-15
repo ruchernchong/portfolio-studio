89a76ed76eb3c4c21e1602e9c2257a1281b19710
Ru Chern Chong <hello@ruchern.xyz>
2023-01-16 01:35:13 +0800

    Test CORS endpoint

--- utils/post-to-dev-community.ts
+++ utils/post-to-dev-community.ts
@@ -1,19 +1,31 @@
 export const postToDevCommunity = (content) => {
-  fetch("https://dev.to/api/articles", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "api-key": import.meta.env.SANITY_STUDIO_DEV_TO_API_KEY,
-    },
-    body: JSON.stringify({
-      article: {
-        title: content.title,
-        body_markdown: content.content,
-        published: true,
-        ...content,
-      },
-    }),
-  })
+  fetch(
+    "https://kqx0owm4v5.execute-api.ap-southeast-1.amazonaws.com/post-to-dev-community",
+    {
+      method: "POST",
+      body: JSON.stringify(content),
+    }
+  )
     .then((res) => res.json())
-    .then((res) => console.log(JSON.stringify(res)));
+    .then((res) => console.log(res))
+    .catch((e) => console.error(e));
+
+  // fetch("https://dev.to/api/articles", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     "api-key": import.meta.env.SANITY_STUDIO_DEV_TO_API_KEY,
+  //   },
+  //   body: JSON.stringify({
+  //     article: {
+  //       title: content.title,
+  //       body_markdown: content.content,
+  //       published: true,
+  //       ...content,
+  //     },
+  //   }),
+  // })
+  //   .then((res) => res.json())
+  //   .then((res) => console.log(JSON.stringify(res)))
+  //   .catch((e) => console.error(e));
 };
