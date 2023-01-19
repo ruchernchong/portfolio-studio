04ebbd7ecddb7d20d4ebb40dd8e723cc52f41e52
Ru Chern Chong <hello@ruchern.xyz>
2023-01-19 23:15:45 +0800

    Clean up

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,21 +1,16 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import { markdownSchema } from "sanity-plugin-markdown";
-import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
 import { SetAndPublishAction } from "./actions/SetAndPublishAction";
 import { CustomMarkdownInput } from "./components/MarkdownInputCustomPreview";
 
 export default defineConfig({
   name: "default",
-  title: "portfolio-studio",
+  title: "Portfolio Studio",
   projectId: "sg29f8lf",
   dataset: "production",
-  plugins: [
-    deskTool(),
-    markdownSchema({ input: CustomMarkdownInput }),
-    visionTool(),
-  ],
+  plugins: [deskTool(), markdownSchema({ input: CustomMarkdownInput })],
   document: {
     actions: (prev) =>
       prev.map((originalAction) =>
