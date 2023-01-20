691ee068efea7f6421b4513f6de588a420e485f5
Ru Chern Chong <hello@ruchern.xyz>
2023-01-20 10:26:54 +0800

    Update URL endpoint

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,6 @@
 interface ImportMetaEnv {
   readonly SANITY_STUDIO_DEV_TO_API_KEY: string;
+  readonly SANITY_STUDIO_DEV_TO_API_URL: string;
 }
 
 interface ImportMeta {
--- utils/post-to-dev-community.ts
+++ utils/post-to-dev-community.ts
@@ -1,31 +1,9 @@
 export const postToDevCommunity = (content) => {
-  fetch(
-    "https://kqx0owm4v5.execute-api.ap-southeast-1.amazonaws.com/post-to-dev-community",
-    {
-      method: "POST",
-      body: JSON.stringify(content),
-    }
-  )
+  fetch(import.meta.env.SANITY_STUDIO_DEV_TO_API_URL, {
+    method: "POST",
+    body: JSON.stringify(content),
+  })
     .then((res) => res.json())
     .then((res) => console.log(res))
     .catch((e) => console.error(e));
-
-  // fetch("https://dev.to/api/articles", {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //     "api-key": import.meta.env.SANITY_STUDIO_DEV_TO_API_KEY,
-  //   },
-  //   body: JSON.stringify({
-  //     article: {
-  //       title: content.title,
-  //       body_markdown: content.content,
-  //       published: true,
-  //       ...content,
-  //     },
-  //   }),
-  // })
-  //   .then((res) => res.json())
-  //   .then((res) => console.log(JSON.stringify(res)))
-  //   .catch((e) => console.error(e));
 };
